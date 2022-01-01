26f128c50f80b1623fb6a0424aa4f76464dc6076
jdhuasirui <jdhuasirui@outlook.com>
2022-01-01 15:51:08 -0500

    add metadata extractor structure and comlete function of image metadata extractor

--- app.py
+++ app.py
@@ -11,6 +11,32 @@ import seaborn as sns
 
 matplotlib.use("Agg")
 
+from datetime import datetime
+# import open files pkgs/metadata extraction
+import os
+# images
+from PIL import Image
+import exifread
+# audio
+import mutagen
+# pdf
+from PyPDF2 import PdfFileReader
+
+# HTML
+matadata_wiki = """
+Metadata is defined as the data providing infomation about one or more aspects of the data.
+"""
+
+HTML_BANNER = """
+    <div style="background-color:#464e5f;padding:10px;border-radius:10px">
+    <h1 style="color:white;text-align:center;">MetaData Extractor App </h1>
+    </div>
+"""
+# define load image function
+def load_image(image_file):
+    img = Image.open(image_file)
+    return img
+
 # import extrernal pkgs
 from app_utils import *
 
@@ -23,10 +49,8 @@ def make_downloadable(data):
     href = f'<a href="data:file/csv;base64,{b64}" download="{new_filename}">Download csv file</a>'
     st.markdown(href, unsafe_allow_html=True)
 
-
 # wordcloud
 from wordcloud import WordCloud
-
 
 def plot_wordcloud(my_text):
     my_wordcloud = WordCloud().generate(my_text)
@@ -92,25 +116,27 @@ def text_analysis(raw_text, num_of_most_common=5):
             except:
                 st.warning("Insufficient Data: Must be more than 2")
 
-
     with st.expander("Download Text Analysis"):
         make_downloadable(token_result_df)
 
+# define function to get human readable time
+def get_readable_time(mytime):
+    return datetime.fromtimestamp(mytime).strftime("%Y-%m-%d-%H:%M")
 
 
 def main():
-    st.title("NLP App with Streamlit")
-    menu = ["Home", "NLP(Files)", "About"]
+    st.title("Streamlit Apps")
+    menu = ["📚NLP", "📚NLP(Upload File)", "Metadata Extractor Home", "Image Metadata Extractor", "Audio Metadata Extractor", "DocumentFiles Metadata Extractor", "About"]
 
     choice = st.sidebar.selectbox("Menu", menu)
-    if choice == "Home":
+    if choice == "📚NLP":
         st.subheader("Home: Text Analysis")
         raw_text = st.text_area("Enter Text Here")
         num_of_most_common = st.sidebar.number_input("Most Common Tokens", 5, 15)
         if st.button("Analyze"):
             text_analysis(raw_text=raw_text, num_of_most_common=num_of_most_common)
 
-    elif choice == "NLP(Files)":
+    elif choice == "📚NLP(Upload File)":
         st.subheader("NLP Task")
 
         text_file = st.file_uploader("Upload Files", type=["pdf", "docx", "txt"])
@@ -125,6 +151,90 @@ def main():
                 raw_text = docx2txt.process(text_file)
 
             text_analysis(raw_text=raw_text, num_of_most_common=num_of_most_common)
+    
+    elif choice == "Metadata Extractor Home":
+        st.subheader('Image Metadata Extractor')
+        # description
+        st.write(matadata_wiki)
+        # create 3 columns
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            with st.expander("Get Image Metadata"):
+                st.info("Image Metadata")
+                st.text("Upload JPEG, JPG, PNG Images")
+        with col2:
+            with st.expander("Get Audio Metadata"):
+                st.info("Audio Metadata")
+                st.text("Upload MP3, Ogg Audios")
+        with col3:
+            with st.expander("Get Document Metadata"):
+                st.info("Document Files Metadata")
+                st.text("Upload PDF, Docx Files")
+
+    elif choice == "Image Metadata Extractor":
+        st.subheader('Image Metadata Extractor')
+        image_file = st.file_uploader("Upload Image",type=["png","jpg","jpeg"])
+        if image_file is not None:
+            with st.expander("File Stats"):
+                file_details = {"FileName":image_file.name,
+                                "FileSize":image_file.size,
+                                "FileType":image_file.type}
+                st.write(file_details)
+
+                statsinfo = os.stat(image_file.readable())
+                st.write(statsinfo)
+                stats_details = {"Accessed_Time":get_readable_time(statsinfo.st_atime),
+                                 "Creation_Time":get_readable_time(statsinfo.st_ctime),
+                                 "Modefication_Time":get_readable_time(statsinfo.st_mtime)}
+                st.write(stats_details)
+
+                # combine all file details
+                file_details_combined = {"FileName":image_file.name,
+                                         "FileSize":image_file.size,
+                                         "FileType":image_file.type,
+                                         "Accessed_Time":get_readable_time(statsinfo.st_atime),
+                                         "Creation_Time":get_readable_time(statsinfo.st_ctime),
+                                         "Modefication_Time":get_readable_time(statsinfo.st_mtime)}
+                
+                # convert to DataFrame
+                df_file_details_combined = pd.DataFrame(file_details_combined.items(),columns=['Meta Tag','Value']).astype(str)
+                st.dataframe(df_file_details_combined)
+            
+            col1, col2 = st.columns(2)
+            with col1:
+                with st.expander("View Image"):
+                    img = load_image(image_file)
+                    st.image(img, width=250)
+            with col2:
+                with st.expander("Default(JPEG"):
+                    st.info("Using Pillow")
+                    img = load_image(image_file)
+                    #st.write(dir(img))
+                    img_details = {"format":img.format,
+                                   "format_description":img.format_description,
+                                   "filename":img.filename,
+                                   "size":img.size,
+                                   "height":img.height,
+                                   "width":img.width,
+                                   "info":img.info}
+                    df_img_details = pd.DataFrame(img_details.items(),columns=['Meta Tag','Value']).astype(str)
+                    st.dataframe(df_img_details)   
+            # layout for forensic
+            fcol1, fcol2 = st.columns(2)
+            with fcol1:
+                with st.expander("Exifread Tool"):
+                    meta_tags = exifread.process_file(image_file)
+                    df_meta_tags = pd.DataFrame(meta_tags.items(),columns=['Meta Tag','Value']).astype(str)
+                    st.dataframe(df_meta_tags) 
+            with fcol2:
+                pass
+
+    elif choice == "Audio Metadata Extractor":
+        st.subheader('Audio Metadata Extractor')
+
+    elif choice == "DocumentFiles Metadata Extractor":
+        st.subheader('DocumentFiles Metadata Extractor Metadata Extractor')
+
     else:
         st.subheader("About")
 
